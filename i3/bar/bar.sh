795a69f1e39396226fe151df148fc803129ca499
Samreen Zarroug <samreen.zarroug@hpe.com>
2017-04-20 10:54:46 -0500

    removed uneeded functions from bar.sh and removed thunner since we won't need these for a vm

--- i3/bar/bar.sh
+++ i3/bar/bar.sh
@@ -5,33 +5,6 @@ clock() {
     DATE=$(date "+%a %m/%d/%y")
 	echo -n "%{F$HIGHLIGHT}$(printf '%b' "\uf073")%{F-} $DATE  %{F$HIGHLIGHT}$(printf '%b' "\uf017")%{F-} $TIME"
 }
-volume() {
-	MUTE=$(amixer get Master | grep % | sed -n 1p | awk -F '[' '{print $4}' | awk -F ']' '{print $1}')
-	VOL=$(amixer get Master | grep % | sed -n 1p | awk -F '[' '{print $2}' | awk -F '%]' '{print $1}')
-	if [ $MUTE = 'off' ] || [ $VOL = 0 ]; then
-		echo -n "%{F$HIGHLIGHT}$(printf '%-b' "\uf026")%{F-}%{O14}Mute"
-	elif (($VOL > 49)); then
-		echo -n "%{F$HIGHLIGHT}$(printf '%-b' "\uf028")%{F-}$(printf ' %03.2d%s' "$VOL" "%")"
-	else
-		echo -n "%{F$HIGHLIGHT}$(printf '%-b' "\uf027")%{F-}%{O4}$(printf ' %03.2d%s' "$VOL" "%")"
-	fi
-}
-wifi() {
-    echo -e "%{F$HIGHLIGHT} \uf1eb %{F-}$(iw wlp2s0 link | grep 'SSID' | cut -c 8-)"
-}
-mail() {
-	# grabs new email # from offlineimap
-		echo -e "%{F$HIGHLIGHT} \uf0e0 %{F-}$(ls /home/ajibola/mail/Gmail/INBOX/new | wc -l)"
-}
-music() {
-	if [ ! -f ~/.thunner.info ]; then
-		echo -n ""
-	else
-		STATUS=$(head -n 1 ~/.thunner.info)
-		SONG=$(tail -n 1 ~/.thunner.info)
-        	echo -n " %{F$HIGHLIGHT}$(printf '%b' $STATUS)%{F-} $SONG "
-	fi
-}
 #Copied from /u/Dylan112
 workspace() {
 	workspacenext="A4:i3-msg workspace next_on_output:"
@@ -80,19 +53,9 @@ bat() {
     fi
 }
 while true; do
-#    echo "%{B#2E3440} $(clock) $(volume) $(mail) $(bat) $(wifi) %{B-}%{c} $(workspace) %{r}%{B-} %{B#3B4252}$(music)%{B#2E3440} $(windowtitle) %{B-}"
-#    echo "%{B#c0303048} $(clock) $(volume) $(mail) $(bat) $(wifi) %{B-}%{c} $(workspace) %{r}%{B-} %{B#3b3b4b}$(music)%{B#c0303048} $(windowtitle) %{B-}"
-#    echo "%{B#c0303048} $(clock) $(volume) $(mail) $(bat) $(wifi) %{B-}%{c} $(workspace) %{r}%{B-} %{B#5d6383}$(music)%{B#c0303048} $(windowtitle) %{B-}"
-    echo "%{B#c0303048}$(workspace)%{B-}$(windowtitle)%{r}$(music)%{B#c0303048}$(volume)  $(bat)  $(clock)%{B-}"
+    echo "%{B#c0303048}$(workspace)%{B-}$(windowtitle)%{r}%{B#c0303048}$(bat)  $(clock)%{B-}"
     sleep .1;
 done |
-# -f '-xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso10646-1'\
-#old one was '-g 1280x20+45'
-#lemonbar -p -d -B#c0262626 -F#A0A0A0 -g 1834x20+45+0\
-# -f '-xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso10646-1'\
-# -f '-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1'\
-# eDP-1\
-# | zsh
 
 lemonbar -p -d -B#c0262626 -F#A0A0A0\
 	-f "FiraMono:size=6"\
