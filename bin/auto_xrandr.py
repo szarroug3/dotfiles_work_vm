911bd4be6bf077e2943af0f863ca3d83547c417e
Samreen Zarroug <szarroug3@gmail.com>
2019-04-17 10:36:47 -0500

    adding panning to autoxrandr

--- bin/auto_xrandr.py
+++ bin/auto_xrandr.py
@@ -1,12 +1,8 @@
-from __future__ import print_function
-
 import argparse
-import ast
-from builtins import range
+import logging
+import os
 import re
 import subprocess
-import sys
-import time
 
 try:
     import configparser
@@ -25,10 +21,12 @@ def read_args():
     parser.add_argument('config', help='path to config file')
     parser.add_argument('-d', '--dry-run', help='Run script as a dry-run', action='store_true')
     parser.add_argument('-f', '--force', help='Run command regardless of whether things changed or not', action='store_true')
+    parser.add_argument('-o', '--offset', help='Default horizontal resolution of monitor to be used for panning offset; will be used only if monitor\'s value is not set in config', default=2560)
+    parser.add_argument('--debug', help='Output debugging statements to ~/auto_xrandr.log', action='store_true')
     return parser.parse_args()
 
 
-def read_config(filename, xrandr):
+def read_config(filename, offset, xrandr, debug):
     """
     Read and validate config with name filename
 
@@ -40,18 +38,20 @@ def read_config(filename, xrandr):
         str: primary monitor
         list: list of monitors to turn on
     """
-    config = configparser.SafeConfigParser({'on': []})
-    config.readfp(open(filename))
+    config = configparser.ConfigParser({'on': []})
+    config.read_file(open(filename))
 
     fail = False
     found = False
     final_primary, final_on = None, None
+    resolutions = {}
 
     for section in config.sections():
         # check to make sure primary is set
         if not config.has_option(section, 'primary'):
             fail = True
-            print('primary cannot be empty in {}'.format(section))
+            if debug:
+                logging.debug('primary cannot be empty in {}'.format(section))
             continue
 
         # check that primary is in on if on is set
@@ -59,18 +59,24 @@ def read_config(filename, xrandr):
         on = [x.strip() for x in config.get(section, 'on').split(',') if x.strip()]
         if on and primary not in on:
             fail = True
-            print('{} must be in on if on is set in {}'.format(primary, section))
+            if debug:
+                logging.debug('{} must be in on if on is set in {}'.format(primary, section))
             continue
 
         # check if all monitors to be turned on are connected
         if validate_monitors(xrandr, primary, on) and not found:
-            final_primary, final_on = primary, on
             found = True
+            final_primary, final_on = primary, on
+            for m in on:
+                resolutions[m] = config.get(section, m, fallback=offset)
 
     if fail:
         raise Exception('Something went wrong')
 
-    return final_primary, final_on
+    if not found:
+        raise Exception('None of the configurations specified in config file worked.')
+
+    return final_primary, final_on, resolutions
 
 
 def read_xrandr():
@@ -84,7 +90,7 @@ def read_xrandr():
         out = subprocess.check_output('xrandr')
         return out.decode('utf-8')
     except OSError:
-        sys.exit('xrandr must be installed')
+        logging.debug('xrandr must be installed')
 
 
 def validate_monitors(xrandr, primary, on):
@@ -115,17 +121,17 @@ def find_enabled_monitors(xrandr):
     return enabled
 
 
-def run_xrandr(primary, on, off, dry_run):
+def run_xrandr(primary, on, off, resolutions, dry_run, debug):
     """
     Compile and execute the xrandr command
 
-    Args:
-        :str primary: primary monitor
+    Args: :str primary: primary monitor
         :list on: list of monitors to turn on
         :list off: list of monitors to turn off
         :bool dry_run: false if running actual commands; true for a dry run
     """
-    command = ['xrandr', '--output', primary, '--primary', '--auto']
+    command = ['xrandr']
+    offset = 0
 
     if primary in off:
         off.remove(primary)
@@ -133,17 +139,21 @@ def run_xrandr(primary, on, off, dry_run):
     if on:
         primary_index = on.index(primary)
         # get monitors left of primary
-        anchor = primary
-        for m in on[:primary_index][::-1]:
-            command.extend(['--output', m, '--left-of', anchor, '--auto'])
-            anchor = m
+        for i, m in enumerate(on[:primary_index]):
+            anchor = on[i+1]
+            command.extend(['--output', m, '--left-of', anchor, '--auto', '--panning', '0x0+{}+0'.format(offset)])
+            offset += resolutions[m]
             if m in off:
                 off.remove(m)
+
+        command.extend(['--output', primary, '--primary', '--auto', '--panning', '0x0+{}+0'.format(offset)])
+        offset += resolutions[primary]
 
         # get monitors right of primary
         anchor = primary
         for m in on[primary_index+1:]:
-            command.extend(['--output', m, '--right-of', anchor, '--auto'])
+            command.extend(['--output', m, '--right-of', anchor, '--auto', '--panning', '0x0+{}+0'.format(offset)])
+            offset += resolutions[m]
             anchor = m
             if m in off:
                 off.remove(m)
@@ -151,26 +161,29 @@ def run_xrandr(primary, on, off, dry_run):
     for m in off:
         command.extend(['--output', m, '--off'])
 
-    print('Executing: {}'.format(' '.join(command)))
+    if dry_run or debug:
+        logging.info('Executing: {}'.format(' '.join(command)))
+
     if not dry_run:
         subprocess.call(command)
 
 
 def main():
+    logging.basicConfig(filename=os.path.join(os.path.expanduser('~'), 'auto_xrandr.log'), level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s: %(message)s')
     args = read_args()
     xrandr = read_xrandr()
     try:
-        primary, on = read_config(args.config, xrandr)
-        if not primary:
-            sys.exit('None of the configurations specified in config file worked.')
+        primary, on, resolutions = read_config(args.config, args.offset, xrandr, args.debug)
     except Exception as e:
-        sys.exit()
+        logging.exception(e)
+        return
 
     off = find_enabled_monitors(xrandr)
 
     # we only want to run the command if something's changed
     if set(on) != set(off) or args.force:
-        run_xrandr(primary, on, off, args.dry_run)
+        run_xrandr(primary, on, off, resolutions, args.dry_run, args.debug)
 
 
 if __name__ == '__main__':
